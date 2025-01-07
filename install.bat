324290c45d9617307bc474899036606e5c62cc94
DogeCN <hark2009lbf@outlook.com>
2025-01-07 17:16:08 +0800

    -

--- install.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-pyinstaller main.spec --noconfirm
-echo 99998 INFO: Updating main.exe
-rem "dist\Plume Lexicon\bin\main.exe"
-copy >nul "dist\installed\main.exe" "dist\Plume Lexicon\bin\"
-echo 99999 INFO: Completed.
