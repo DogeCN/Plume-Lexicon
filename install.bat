d5170abfe735abf606138c8986ac4230d1c2533f
DogeCN <hark2009lbf@outlook.com>
2025-01-07 17:12:41 +0800

    修改安装脚本

--- install.bat
+++ install.bat
@@ -1,4 +1,6 @@
 @echo off
 pyinstaller main.spec --noconfirm
-
-echo 99999 INFO: Plume Lexicon.exe Completed.
+echo 99998 INFO: Updating main.exe
+rem "dist\Plume Lexicon\bin\main.exe"
+copy >nul "dist\installed\main.exe" "dist\Plume Lexicon\bin\"
+echo 99999 INFO: Completed.
